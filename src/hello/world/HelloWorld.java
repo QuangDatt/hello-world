b2981d356de1cd691abd8da7893dea2bd89f41a8
QuangDatt <dieisno@gmail.com>
2022-05-24 11:29:26 +0700

    li-do-dua-len-la-gi-ta-co-sua-gi-o-code

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world;
+
+/**
+ *
+ * @author SE150571 Tran Quang Dat
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Weclcom to SE1602 , GitHUb is so fun!!!");
+    }
+    
+}
